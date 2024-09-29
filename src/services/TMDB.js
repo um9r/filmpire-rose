20d203ae0cf22dd559c04bc95e2a6c04a674e96a
Umar <faroukumar99@gmail.com>
2024-09-29 22:59:06 +0100

    assets

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -8,6 +8,11 @@ export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
   endpoints: (builder) => ({
+    //* Get Genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`
+    }),
+
     //* Get Movies by [Type]
     getMovies: builder.query({
       query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
@@ -15,4 +20,4 @@ export const tmdbApi = createApi({
   }),
 });
 
-export const { useGetMoviesQuery } = tmdbApi;
+export const { useGetMoviesQuery, useGetGenresQuery } = tmdbApi;

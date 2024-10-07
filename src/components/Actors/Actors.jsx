83b4bc3ae1183843c5da478d7007f2853c5d8427
Umar <faroukumar99@gmail.com>
2024-10-07 22:39:18 +0100

    pagination

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -3,12 +3,74 @@ import { Box, Button, CircularProgress, Grid2, Typography } from '@mui/material'
 import { useHistory, useParams } from 'react-router-dom';
 import { ArrowBack } from '@mui/icons-material';
 
+import useStyles from './styles'
+import { useGetActorsDetailsQuery, useGetMovieByActorIdQuery } from '../../services/TMDB';
+import { MovieList, Pagination } from '..';
+
 const Actors = () => {
-    const { id } = useParams()
+    const { id } = useParams();
+    const history = useHistory();
+    const classes = useStyles();
+    const [page, setPage] = useState(1);
+
+    const { data, isFetching, error } = useGetActorsDetailsQuery(id);
+    const { data: movies } = useGetMovieByActorIdQuery({ id, page })
+
+    if (isFetching) {
+      return (
+        <Box display='flex' justifyContent='center'>
+          <CircularProgress size='8rem' />
+        </Box>
+      );
+    }
+    if (error) {
+      return (
+        <Box display='flex' justifyContent='center' alignItems='center'>
+          <Button startIcon={<ArrowBack />} onClick={() => history.goBack()} color='primary'>
+            Go back
+          </Button>
+        </Box>
+      );
+    }
+
   return (
-    <div>
-      Actors - {id}
-    </div>
+    <>
+      <Grid2 container spacing={3}>
+        <Grid2 item lg={5} xl={4}>
+          <img
+          className={classes.image}
+          src={`https://image.tmdb.org/t/p/w780/${data?.profile_path}`}
+          alt={data.name}
+          />
+        </Grid2>
+        <Grid2 item lg={7} xl={8} style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+          <Typography variant='h2' gutterBottom>
+            {data?.name}
+          </Typography>
+          <Typography variant='h5' gutterBottom>
+           Born: {new Date(data?.birthday).toDateString()}
+          </Typography>
+          <Typography variant='body1' align='justify' component='p'>
+            {data?.biography || 'Sorry, no biography yet...'}
+          </Typography>
+          <Box marginTop='2rem' display='flex' justifyContent='space-around'>
+            <Button variant='contained' color='primary' target='_blank' href={`https://www.imdb.com/name/${data?.imdb_id}`}>
+              IMDB
+            </Button>
+            <Button startIcon={<ArrowBack />} onClick={() => history.goBack()} color='primary'>
+              Back
+            </Button>
+          </Box>
+        </Grid2>
+      </Grid2>
+      <Box margin='2rem 0'>
+        <Typography variant='h2' gutterBottom align='center'>
+          Movies
+        </Typography>
+        {movies && <MovieList movies={movies} numberOfMovies={8} />}
+        <Pagination currentPage={page} setPage={setPage} totalPages={movies?.total_pages} />
+      </Box>
+    </>
   )
 }
 
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -9,7 +9,7 @@ export const tmdbApi = createApi({
   endpoints: (builder) => ({
     //* Get Genres
     getGenres: builder.query({
-      query: () => `genre/movie/list?api_key=${tmdbApiKey}`
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
     }),
 
     //* Get Movies by [Type]
@@ -17,34 +17,57 @@ export const tmdbApi = createApi({
       query: ({ genreIdOrCategoryName, page, searchQuery }) => {
         //* Get Movies by Search
         if (searchQuery) {
-          return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`
+          return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
         }
 
         //* Get Movies by Category
-        if ( genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
-          return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`
+        if (
+          genreIdOrCategoryName &&
+          typeof genreIdOrCategoryName === "string"
+        ) {
+          return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
         }
 
         //* Get Movies by Genre
-        if ( genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
-          return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`
+        if (
+          genreIdOrCategoryName &&
+          typeof genreIdOrCategoryName === "number"
+        ) {
+          return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
         }
 
         //* Get Popular Movies
-        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`
+        return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
       },
     }),
 
     //* Get Movie
     getMovie: builder.query({
-      query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`
+      query: (id) =>
+        `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
     }),
 
     //* Get User Specific Lists
     getRecommendations: builder.query({
-  query: (movie_id) => `/movie/${movie_id}/recommendations?api_key=${tmdbApiKey}`
-  }),
+      query: (movie_id) =>
+        `/movie/${movie_id}/recommendations?api_key=${tmdbApiKey}`,
+    }),
+
+    getActorsDetails: builder.query({
+      query: (id) => `person/${id}?api_key=${tmdbApiKey}`,
+    }),
+
+    getMovieByActorId: builder.query({
+      query: ({id, page}) => `/discover/movie?with_cast=${id}&page=${page}&api_key=${tmdbApiKey}`
+    }),
   }),
 });
 
-export const { useGetMoviesQuery, useGetGenresQuery, useGetMovieQuery, useGetRecommendationsQuery } = tmdbApi;
+export const {
+  useGetMoviesQuery,
+  useGetGenresQuery,
+  useGetMovieQuery,
+  useGetRecommendationsQuery,
+  useGetActorsDetailsQuery,
+  useGetMovieByActorIdQuery,
+} = tmdbApi;
